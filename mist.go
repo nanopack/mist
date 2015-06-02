e7eede04db452b9d712e29918455ceeaba27f229
sdomino <sdomino@pagodabox.com>
2015-06-02 08:44:24 -0600

    updating licensing

--- mist.go
+++ mist.go
@@ -1,10 +1,17 @@
+// Copyright (c) 2015 Pagoda Box Inc
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v.
+// 2.0. If a copy of the MPL was not distributed with this file, You can obtain one
+// at http://mozilla.org/MPL/2.0/.
+//
+
 package mist
 
 import (
 	"fmt"
 	"sync"
 
-	"github.com/nanobox-core/hatchet"
+	"github.com/pagodabox/golang-hatchet"
 )
 
 //
