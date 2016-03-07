2aca01b0c1ab819cfd5d07055297805b5fea2714
Steve Domino <domino.steve@gmail.com>
2016-03-07 14:44:53 -0700

    added build script and updated .gitignore to not include build files

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+
+# remove any previous builds that may have failed
+[ -e "./build" ] && \
+  echo "Cleaning up old builds..." && \
+  rm -rf "./build"
+
+# build mist
+echo "Building MIST and uploading it to 's3://tools.nanopack.io/mist'"
+gox -osarch "darwin/amd64 linux/amd64 windows/amd64" -output="./build/{{.OS}}/{{.Arch}}/mist"
+
+# look through each os/arch/file and generate an md5 for each
+echo "Generating md5s..."
+for os in $(ls ./build); do
+  for arch in $(ls ./build/${os}); do
+    for file in $(ls ./build/${os}/${arch}); do
+      cat "./build/${os}/${arch}/${file}" | md5 >> "./build/${os}/${arch}/${file}.md5"
+    done
+  done
+done
+
+# upload to AWS S3
+echo "Uploading builds to S3..."
+aws s3 sync ./build/ s3://tools.nanopack.io/mist --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --region us-east-1
+
+#
+echo "Cleaning up..."
+
+# remove build
+[ -e "./build" ] && \
+  echo "Removing build files..." && \
+  rm -rf "./build"
