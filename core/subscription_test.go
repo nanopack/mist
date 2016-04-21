2df948bdd70049b457e684605072480718e1c0fb
Steve Domino <domino.steve@gmail.com>
2016-04-21 08:29:40 -0600

    cleaning up subs

--- core/subscription_test.go
+++ core/subscription_test.go
@@ -1,167 +1,365 @@
 package mist
 
-import "testing"
+import (
+	"sort"
+	"testing"
+)
 
-// BenchmarkAddRemoveKeys benchmarks to see how fast mist can add/remove keys to
-// a subscription
-func BenchmarkAddRemoveKeys(b *testing.B) {
-	sub := newSub()
+type (
 
-	// create a giant slice of random keys
-	keys := [][]string{}
-	for i := 0; i < b.N; i++ {
-		keys = append(keys, []string{randKey(), randKey(), randKey(), randKey(), randKey(), randKey(), randKey(), randKey()})
+	//
+	subTest struct {
+		test   []string
+		result bool
 	}
 
-	b.ResetTimer()
-
-	// add/remove keys
-	for _, k := range keys {
-		sub.Add(k)
-		sub.Remove(k)
+	//
+	matchTest struct {
+		sub   []string
+		tests []subTest
 	}
-}
+)
 
-// BenchmarkMatch benchmarks to see how fast mist can match a set of keys on a
-// subscription
-func BenchmarkMatch(b *testing.B) {
-	sub := newSub()
-
-	// create a giant slice of random keys
-	keys := [][]string{}
-	for i := 0; i < b.N; i++ {
-		keys = append(keys, []string{randKey(), randKey(), randKey(), randKey(), randKey(), randKey(), randKey(), randKey()})
-	}
-
-	b.ResetTimer()
-
-	// add/match keys
-	for _, k := range keys {
-		sub.Add(k)
-		sub.Match(k)
-	}
-}
-
-// TestEmptySubscription tests to ensure a new subscription is empty
+// TestEmptySubscription
 func TestEmptySubscription(t *testing.T) {
-	sub := newSub()
-	if len(sub) != 0 {
-		t.Fatalf("Unexpected tags in new subscription!")
+	node := newNode()
+	if node.Len() != 0 {
+		t.Fatalf("empty subscripition should be empty")
 	}
 }
 
-// TestAddRemove tests to ensure that adding/removing tags works as expected
+// TestAddRemove
 func TestAddRemove(t *testing.T) {
-	sub := newSub()
+	node := newNode()
 
-	// add a single tag and remove it
-	sub.Add([]string{"a"})
-	if len(sub) != 1 {
-		t.Fatalf("Unexpected number of tags - Expecting %v got %v", 1, len(sub))
-	}
-	sub.Remove([]string{"a"})
-	if len(sub) != 0 {
-		t.Fatalf("Unexpected number of tags - Expecting %v got %v", 0, len(sub))
+	//
+	node.Add([]string{"a", "b"})
+	if node.Len() != 1 {
+		t.Fatalf("add should have added a node")
 	}
 
-	// add multiple tags and remove them
-	sub.Add([]string{"a", "b"})
-	if len(sub) != 1 {
-		t.Fatalf("Unexpected number of tags - Expecting %v got %v", 1, len(sub))
-	}
-	sub.Remove([]string{"a", "b"})
-	if len(sub) != 0 {
-		t.Fatalf("Unexpected number of tags - Expecting %v got %v", 0, len(sub))
+	//
+	node.Remove([]string{"a", "b"})
+	if node.Len() != 0 {
+		t.Fatalf("remove should have removed a node")
 	}
 
-	// add multiple tags and attempt to remove them unordered
-	sub.Add([]string{"a", "b"})
-	if len(sub) != 1 {
-		t.Fatalf("Unexpected number of tags - Expecting %v got %v", 1, len(sub))
+	//
+	node.Add([]string{"a", "b"})
+	if node.Len() != 1 {
+		t.Fatalf("add should have added a node")
 	}
-	sub.Remove([]string{"b", "a"})
-	if len(sub) != 0 {
-		t.Fatalf("Unexpected number of tags - Expecting %v got %v", 0, len(sub))
+
+	//
+	node.Remove([]string{"b", "a"})
+	if node.Len() != 0 {
+		t.Fatalf("remove should have removed a node")
 	}
 }
 
-// TestList ensure listing tags on a subscription works as expected
+// TestList
 func TestList(t *testing.T) {
+	node := newNode()
 
-	sub := newSub()
-
-	var list string
-
-	// list single tag
-	sub.Add([]string{"a"})
-	list = flattenSliceToString(sub.ToSlice())
-	if list != "a" {
-		t.Fatalf("Unexpected tag - Expecting %v got %v", "a", list)
+	//
+	node.Add([]string{"a", "b"})
+	list := node.ToSlice()
+	if len(list) != 1 {
+		t.Fatalf("wrong length of list. Expecting 1 got %v", list)
 	}
-	sub.Remove([]string{"a"})
-
-	// list compound tags
-	sub.Add([]string{"a", "b"})
-	list = flattenSliceToString(sub.ToSlice())
-	if list != "a,b" {
-		t.Fatalf("Unexpected tags - Expecting %v got %v", "a,b", list)
-	}
-	sub.Remove([]string{"a", "b"})
-
-	// list multiple tags; we test both configurations here because maps are unordered
-	sub.Add([]string{"a", "b"})
-	sub.Add([]string{"c"})
-	list = flattenSliceToString(sub.ToSlice())
-	switch list {
-	case "a,bc", "ca,b":
-	default:
-		t.Fatalf("Unexpected tags - Expecting %v got %v", "'a,bc' OR 'ca,b'", list)
-	}
-	sub.Remove([]string{"a", "b"})
-	sub.Remove([]string{"c"})
-
-	// all tags should be removed
-	tags := sub.ToSlice()
-	if len(tags) != 0 {
-		t.Fatalf("Unexpected length of tags - Expecting %v got %v", 0, len(tags))
+	if len(list[0]) != 2 {
+		t.Fatalf("wrong numer of tags. Expecing 2 got %v", len(list[0]))
 	}
 }
 
-// TestAddRemoveDuplicate tests to ensure that adding duplicate tags don't actually
-// get added to the subscription
+// TestAddRemoveDuplicate
 func TestAddRemoveDuplicate(t *testing.T) {
-	sub := newSub()
+	node := newNode()
 
-	// add dupicate tags and unordered duplicates
-	sub.Add([]string{"a", "b"})
-	sub.Add([]string{"a", "b"})
-	sub.Add([]string{"b", "a"})
-	if len(sub) != 1 {
-		t.Fatalf("Unexpected number of tags - Expecting %v got %v", 1, len(sub))
+	//
+	node.Add([]string{"a", "b"})
+	node.Add([]string{"a", "b"})
+	if node.Len() != 1 {
+		t.Fatalf("duplicate add should not have added a node")
 	}
 
-	// remove tags; multiple removes should have no effect
-	sub.Remove([]string{"a", "b"})
-	sub.Remove([]string{"a", "b"})
-	sub.Remove([]string{"b", "a"})
-	if len(sub) != 0 {
-		t.Fatalf("Unexpected number of tags - Expecting %v got %v", 0, len(sub))
+	//
+	node.Remove([]string{"a", "b"})
+	if node.Len() != 1 {
+		t.Fatalf("duplicate remove should not have removed a node with a count > 1")
+	}
+
+	//
+	node.Remove([]string{"a", "b"})
+	if node.Len() != 0 {
+		t.Fatalf("duplicate remove should have removed a node")
+	}
+}
+
+// TestAddRemoveSimilarDuplicate
+func TestAddRemoveSimilarDuplicate(t *testing.T) {
+	node := newNode()
+
+	//
+	node.Add([]string{"a", "b"})
+	if node.Len() != 1 {
+		t.Fatalf("similar duplicate add should have added a node")
+	}
+
+	//
+	node.Add([]string{"a", "b", "c"})
+	if node.Len() != 2 {
+		t.Fatalf("similar duplicate add should not have added a node")
+	}
+
+	//
+	node.Remove([]string{"a", "b"})
+	if node.Len() != 1 {
+		t.Fatalf("similar duplicate remove should not have removed a node with a count > 1")
+	}
+
+	//
+	node.Remove([]string{"a", "b", "c"})
+	if node.Len() != 0 {
+		t.Fatalf("similar duplicate remove should have removed a node")
 	}
 }
 
-// TestMatch
-func TestMatch(t *testing.T) {
-	sub := newSub()
-
-	// add a single tag and test match
-	sub.Add([]string{"a"})
-	if !sub.Match([]string{"a"}) {
-		t.Fatalf("Expected match!")
+// TestMatches
+func TestMatches(t *testing.T) {
+	matches := []matchTest{
+		{
+			[]string{},
+			[]subTest{
+				{[]string{"a"}, false},
+				{[]string{}, false},
+			},
+		},
+		{
+			[]string{"a", "b"},
+			[]subTest{
+				{[]string{}, false},
+				{[]string{"a", "b"}, true},
+				{[]string{"b", "a"}, true},
+				{[]string{"b", "a", "c"}, true},
+				{[]string{"c", "a", "b"}, true},
+				{[]string{"c", "a"}, false},
+				{[]string{"c", "b"}, false},
+				{[]string{"c"}, false},
+			},
+		},
 	}
 
-	// add multiple tags and test for a match against unordered tags
-	sub.Add([]string{"a", "b"})
-	if !sub.Match([]string{"b", "a"}) {
-		t.Fatalf("Expected match!")
+	for _, match := range matches {
+		node := newNode()
+		sort.Sort(sort.StringSlice(match.sub))
+
+		//
+		node.Add(match.sub)
+
+		//
+		for _, mt := range match.tests {
+			sort.Sort(sort.StringSlice(mt.test))
+			if node.Match(mt.test) != mt.result {
+				t.Fatalf("match failed: %v:%v expected %v", match.sub, mt.test, mt.result)
+			}
+		}
+
+		//
+		node.Remove(match.sub)
+
+		//
+		for _, mt := range match.tests {
+			if node.Match(mt.test) {
+				t.Fatalf("match failed: []:%v expected false", mt.test)
+			}
+		}
+
 	}
 }
+
+// BenchmarkAddRemove
+func BenchmarkAddRemove(b *testing.B) {
+	node := newNode()
+	keys := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		node.Add(keys)
+		node.Remove(keys)
+	}
+}
+
+// BenchmarkMatch
+func BenchmarkMatch(b *testing.B) {
+	node := newNode()
+	keys := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
+	node.Add(keys)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		node.Match(keys)
+	}
+}
+
+// // BenchmarkAddRemoveKeys benchmarks to see how fast mist can add/remove keys to
+// // a subscription
+// func BenchmarkAddRemoveKeys(b *testing.B) {
+// 	sub := newSub()
+//
+// 	// create a giant slice of random keys
+// 	keys := [][]string{}
+// 	for i := 0; i < b.N; i++ {
+// 		keys = append(keys, []string{randKey(), randKey(), randKey(), randKey(), randKey(), randKey(), randKey(), randKey()})
+// 	}
+//
+// 	b.ResetTimer()
+//
+// 	// add/remove keys
+// 	for _, k := range keys {
+// 		sub.Add(k)
+// 		sub.Remove(k)
+// 	}
+// }
+//
+// // BenchmarkMatch benchmarks to see how fast mist can match a set of keys on a
+// // subscription
+// func BenchmarkMatch(b *testing.B) {
+// 	sub := newSub()
+//
+// 	// create a giant slice of random keys
+// 	keys := [][]string{}
+// 	for i := 0; i < b.N; i++ {
+// 		keys = append(keys, []string{randKey(), randKey(), randKey(), randKey(), randKey(), randKey(), randKey(), randKey()})
+// 	}
+//
+// 	b.ResetTimer()
+//
+// 	// add/match keys
+// 	for _, k := range keys {
+// 		sub.Add(k)
+// 		sub.Match(k)
+// 	}
+// }
+//
+// // TestEmptySubscription tests to ensure a new subscription is empty
+// func TestEmptySubscription(t *testing.T) {
+// 	sub := newSub()
+// 	if len(sub) != 0 {
+// 		t.Fatalf("Unexpected tags in new subscription!")
+// 	}
+// }
+//
+// // TestAddRemove tests to ensure that adding/removing tags works as expected
+// func TestAddRemove(t *testing.T) {
+// 	sub := newSub()
+//
+// 	// add a single tag and remove it
+// 	sub.Add([]string{"a"})
+// 	if len(sub) != 1 {
+// 		t.Fatalf("Unexpected number of tags - Expecting %v got %v", 1, len(sub))
+// 	}
+// 	sub.Remove([]string{"a"})
+// 	if len(sub) != 0 {
+// 		t.Fatalf("Unexpected number of tags - Expecting %v got %v", 0, len(sub))
+// 	}
+//
+// 	// add multiple tags and remove them
+// 	sub.Add([]string{"a", "b"})
+// 	if len(sub) != 1 {
+// 		t.Fatalf("Unexpected number of tags - Expecting %v got %v", 1, len(sub))
+// 	}
+// 	sub.Remove([]string{"a", "b"})
+// 	if len(sub) != 0 {
+// 		t.Fatalf("Unexpected number of tags - Expecting %v got %v", 0, len(sub))
+// 	}
+//
+// 	// add multiple tags and attempt to remove them unordered
+// 	sub.Add([]string{"a", "b"})
+// 	if len(sub) != 1 {
+// 		t.Fatalf("Unexpected number of tags - Expecting %v got %v", 1, len(sub))
+// 	}
+// 	sub.Remove([]string{"b", "a"})
+// 	if len(sub) != 0 {
+// 		t.Fatalf("Unexpected number of tags - Expecting %v got %v", 0, len(sub))
+// 	}
+// }
+//
+// // TestList ensure listing tags on a subscription works as expected
+// func TestList(t *testing.T) {
+//
+// 	sub := newSub()
+//
+// 	var list string
+//
+// 	// list single tag
+// 	sub.Add([]string{"a"})
+// 	list = flattenSliceToString(sub.ToSlice())
+// 	if list != "a" {
+// 		t.Fatalf("Unexpected tag - Expecting %v got %v", "a", list)
+// 	}
+// 	sub.Remove([]string{"a"})
+//
+// 	// list compound tags
+// 	sub.Add([]string{"a", "b"})
+// 	list = flattenSliceToString(sub.ToSlice())
+// 	if list != "a,b" {
+// 		t.Fatalf("Unexpected tags - Expecting %v got %v", "a,b", list)
+// 	}
+// 	sub.Remove([]string{"a", "b"})
+//
+// 	// list multiple tags; we test both configurations here because maps are unordered
+// 	sub.Add([]string{"a", "b"})
+// 	sub.Add([]string{"c"})
+// 	list = flattenSliceToString(sub.ToSlice())
+// 	switch list {
+// 	case "a,bc", "ca,b":
+// 	default:
+// 		t.Fatalf("Unexpected tags - Expecting %v got %v", "'a,bc' OR 'ca,b'", list)
+// 	}
+// 	sub.Remove([]string{"a", "b"})
+// 	sub.Remove([]string{"c"})
+//
+// 	// all tags should be removed
+// 	tags := sub.ToSlice()
+// 	if len(tags) != 0 {
+// 		t.Fatalf("Unexpected length of tags - Expecting %v got %v", 0, len(tags))
+// 	}
+// }
+//
+// // TestAddRemoveDuplicate tests to ensure that adding duplicate tags don't actually
+// // get added to the subscription
+// func TestAddRemoveDuplicate(t *testing.T) {
+// 	sub := newSub()
+//
+// 	// add dupicate tags and unordered duplicates
+// 	sub.Add([]string{"a", "b"})
+// 	sub.Add([]string{"a", "b"})
+// 	sub.Add([]string{"b", "a"})
+// 	if len(sub) != 1 {
+// 		t.Fatalf("Unexpected number of tags - Expecting %v got %v", 1, len(sub))
+// 	}
+//
+// 	// remove tags; multiple removes should have no effect
+// 	sub.Remove([]string{"a", "b"})
+// 	sub.Remove([]string{"a", "b"})
+// 	sub.Remove([]string{"b", "a"})
+// 	if len(sub) != 0 {
+// 		t.Fatalf("Unexpected number of tags - Expecting %v got %v", 0, len(sub))
+// 	}
+// }
+//
+// // TestMatch
+// func TestMatch(t *testing.T) {
+// 	sub := newSub()
+//
+// 	// add a single tag and test match
+// 	sub.Add([]string{"a"})
+// 	if !sub.Match([]string{"a"}) {
+// 		t.Fatalf("Expected match!")
+// 	}
+//
+// 	// add multiple tags and test for a match against unordered tags
+// 	sub.Add([]string{"a", "b"})
+// 	if !sub.Match([]string{"b", "a"}) {
+// 		t.Fatalf("Expected match!")
+// 	}
+// }
--- core/subscriptions.go
+++ core/subscriptions.go
@@ -1,8 +1,9 @@
 package mist
 
-import (
-	"sort"
-	"strings"
+const (
+	create = iota
+	remove
+	nothing
 )
 
 // interfaces
@@ -20,66 +21,196 @@ type (
 // structs
 type (
 
+	// Node ...
+	Node struct {
+		id       uint64
+		key      string
+		count    int
+		children map[string]*Node
+		parent   *Node
+		leafs    map[uint64]*Node
+	}
+
 	// Sub ...
-	Sub map[string]struct{}
+	// Sub map[string]struct{}
 )
 
-// newSub creates a new subscription
-func newSub() (sub Sub) {
-	return make(Sub)
-}
+// // newSub creates a new subscription
+// func newSub() (sub Sub) {
+// 	return make(Sub)
+// }
+//
+// // Add adds tags to the subscription
+// func (sub Sub) Add(tags []string) {
+//
+// 	// sort tags
+// 	sort.Strings(tags)
+//
+// 	// join them into a string
+// 	chk := strings.Join(tags, ",")
+//
+// 	// see if the key is in the map; if not insert it
+// 	if _, found := sub[chk]; !found {
+// 		sub[chk] = struct{}{}
+// 	}
+// }
+//
+// // Remove removes tags from the subscription
+// func (sub Sub) Remove(tags []string) {
+//
+// 	// sort tags
+// 	sort.Strings(tags)
+//
+// 	// delete tags
+// 	delete(sub, strings.Join(tags, ","))
+// }
+//
+// // Match checks to see if the subscription has tags
+// func (sub Sub) Match(tags []string) (found bool) {
+//
+// 	// sort tags
+// 	sort.Strings(tags)
+//
+// 	// see if the key is in the map
+// 	_, found = sub[strings.Join(tags, ",")]
+//
+// 	//
+// 	return
+// }
+//
+// // ToSlice converts the subscription (map) to a slice of tags (slices)
+// func (sub Sub) ToSlice() (slice [][]string) {
+//
+// 	// for each key in sub...
+// 	for k := range sub {
+//
+// 		// split the key into a slice...
+// 		split := strings.Split(k, ",")
+//
+// 		// ...and append that slice to the final
+// 		slice = append(slice, split)
+// 	}
+//
+// 	//
+// 	return
+// }
 
-// Add adds tags to the subscription
-func (sub Sub) Add(tags []string) {
-
-	// sort tags
-	sort.Strings(tags)
-
-	// join them into a string
-	chk := strings.Join(tags, ",")
-
-	// see if the key is in the map; if not insert it
-	if _, found := sub[chk]; !found {
-		sub[chk] = struct{}{}
+//
+func newNode() (child *Node) {
+	child = &Node{
+		id:       0,
+		count:    0,
+		children: map[string]*Node{},
+		leafs:    map[uint64]*Node{},
 	}
-}
-
-// Remove removes tags from the subscription
-func (sub Sub) Remove(tags []string) {
-
-	// sort tags
-	sort.Strings(tags)
-
-	// delete tags
-	delete(sub, strings.Join(tags, ","))
-}
-
-// Match checks to see if the subscription has tags
-func (sub Sub) Match(tags []string) (found bool) {
-
-	// sort tags
-	sort.Strings(tags)
-
-	// see if the key is in the map
-	_, found = sub[strings.Join(tags, ",")]
 
 	//
 	return
 }
 
-// ToSlice converts the subscription (map) to a slice of tags (slices)
-func (sub Sub) ToSlice() (slice [][]string) {
+// Len ...
+func (root *Node) Len() int {
+	return len(root.leafs)
+}
 
-	// for each key in sub...
-	for k := range sub {
+// Add ...
+func (root *Node) Add(keys []string) {
+	if len(keys) == 0 {
+		return
+	}
+	last, _ := root.traverse(keys, create)
+	last.count++
+	if last.count == 1 {
+		last.id = root.id
+		root.leafs[last.id] = last
+		root.id++
+	}
+}
 
-		// split the key into a slice...
-		split := strings.Split(k, ",")
+// Remove ...
+func (root *Node) Remove(keys []string) {
+	if len(keys) == 0 {
+		return
+	}
+	found, _ := root.traverse(keys, remove)
+	if found != nil {
+		found.count--
+		if found.count == 0 {
+			delete(root.leafs, found.id)
+		}
+	}
+}
 
-		// ...and append that slice to the final
-		slice = append(slice, split)
+// Match ...
+func (root *Node) Match(keys []string) bool {
+	last, count := root.traverse(keys, nothing)
+	return last != nil && count != -1
+}
+
+// ToSlice ...
+func (root *Node) ToSlice() [][]string {
+	paths := make([][]string, len(root.leafs))
+	for idx, leaf := range root.leafs {
+		var path []string
+		for ; leaf != nil && leaf != root; leaf = leaf.parent {
+			path = append(path, leaf.key)
+		}
+		paths[idx] = path
+	}
+	return paths
+}
+
+// Find ...
+func (root *Node) Find(keys []string) *Node {
+	child, _ := root.traverse(keys, nothing)
+	return child
+}
+
+// traverse ...
+func (root *Node) traverse(keys []string, action int) (*Node, int) {
+	if len(keys) == 0 {
+		if root.count == 0 {
+			// this node is not a leaf, so return -1 so it doesn't get deleted
+			return root, -1
+		}
+		return root, root.count
 	}
 
-	//
-	return
+	key := keys[0]
+	child, ok := root.children[key]
+
+	switch action {
+	case remove:
+		if ok {
+			found, count := child.traverse(keys[1:], action)
+			if found != nil && count == 1 {
+				if child.count == 0 && len(child.children) == 0 {
+					delete(root.children, key)
+				}
+			}
+			return found, count
+		}
+
+		return nil, 0
+	case create:
+		if !ok {
+			child = newNode()
+			child.parent = root
+			child.key = keys[0] // preserve the original key
+			root.children[key] = child
+		}
+
+		return child.traverse(keys[1:], action)
+	default:
+		if ok {
+			found, count := child.traverse(keys[1:], action)
+			// if 0 or -1 indicate that the traversal didn't really work
+			if count != 0 {
+				return found, count
+			}
+		}
+
+		// we didn't find a match with the key, try the rest of the keys
+		return root.traverse(keys[1:], action)
+	}
 }
