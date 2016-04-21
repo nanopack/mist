bb8773c49428497f07e0b3cffa34f33563ec6ce8
Steve Domino <domino.steve@gmail.com>
2016-04-21 15:35:45 -0600

    reverting subscriptions back to a similar way to what was there before, but a little simpler. updating tests

--- core/subscription_test.go
+++ core/subscription_test.go
@@ -1,59 +1,122 @@
 package mist
 
 import (
-	"sort"
+	"strings"
 	"testing"
 )
 
-type (
-
-	//
-	subTest struct {
-		test   []string
-		result bool
-	}
-
-	//
-	matchTest struct {
-		sub   []string
-		tests []subTest
-	}
-)
+// // BenchmarkAddRemoveKeys benchmarks to see how fast mist can add/remove keys to
+// // a subscription
+// func BenchmarkAddRemoveKeys(b *testing.B) {
+// 	node := newNode()
+//
+// 	// create a giant slice of random keys
+// 	keys := [][]string{}
+// 	for i := 0; i < b.N; i++ {
+// 		keys = append(keys, []string{randKey(), randKey(), randKey(), randKey(), randKey(), randKey(), randKey(), randKey()})
+// 	}
+//
+// 	b.ResetTimer()
+//
+// 	// add/remove keys
+// 	for _, k := range keys {
+// 		node.Add(k)
+// 		node.Remove(k)
+// 	}
+// }
+//
+// // BenchmarkMatch benchmarks to see how fast mist can match a set of keys on a
+// // subscription
+// func BenchmarkMatch(b *testing.B) {
+// 	node := newNode()
+//
+// 	// create a giant slice of random keys
+// 	keys := [][]string{}
+// 	for i := 0; i < b.N; i++ {
+// 		keys = append(keys, []string{randKey(), randKey(), randKey(), randKey(), randKey(), randKey(), randKey(), randKey()})
+// 	}
+//
+// 	b.ResetTimer()
+//
+// 	// add/match keys
+// 	for _, k := range keys {
+// 		node.Add(k)
+// 		node.Match(k)
+// 	}
+// }
 
 // TestEmptySubscription
 func TestEmptySubscription(t *testing.T) {
 	node := newNode()
-	if node.Len() != 0 {
-		t.Fatalf("empty subscripition should be empty")
+	if len(node.ToSlice()) != 0 {
+		t.Fatalf("Unexpected tags in new subscription!")
 	}
 }
 
-// TestAddRemove
-func TestAddRemove(t *testing.T) {
+// TestAddRemoveSimple
+func TestAddRemoveSimple(t *testing.T) {
 	node := newNode()
 
 	//
-	node.Add([]string{"a", "b"})
-	if node.Len() != 1 {
-		t.Fatalf("add should have added a node")
+	node.Add([]string{"a"})
+	if len(node.ToSlice()) != 1 {
+		t.Fatalf("Failed to add node")
 	}
 
-	//
+	node.Remove([]string{"a"})
+	if len(node.ToSlice()) != 0 {
+		t.Fatalf("Failed to remove node")
+	}
+}
+
+// TestAddRemoveComplex
+func TestAddRemoveComplex(t *testing.T) {
+	node := newNode()
+
+	// add/remove unordered keys; should remove
+	node.Add([]string{"a", "b", "c"})
+	node.Remove([]string{"c", "b", "a"})
+	if len(node.ToSlice()) != 0 {
+		t.Fatalf("Failed to remove node")
+	}
+
+	// add/remove incomplete keys; should not remove
+	node.Add([]string{"a", "b", "c"})
+	node.Remove([]string{"a"})
+	node.Remove([]string{"b"})
+	node.Remove([]string{"c"})
 	node.Remove([]string{"a", "b"})
-	if node.Len() != 0 {
-		t.Fatalf("remove should have removed a node")
+	node.Remove([]string{"b", "c"})
+	node.Remove([]string{"a", "c"})
+	node.Remove([]string{"b", "c", "d"})
+	node.Remove([]string{"a", "b", "c", "d"})
+	if len(node.ToSlice()) != 1 {
+		t.Fatalf("Node unexpectedly removed")
 	}
 
-	//
-	node.Add([]string{"a", "b"})
-	if node.Len() != 1 {
-		t.Fatalf("add should have added a node")
+	// add duplicate keys; should only add once
+	node.Add([]string{"a", "b", "c"})
+	node.Add([]string{"a", "b", "c"})
+	if len(node.ToSlice()) != 1 {
+		t.Fatalf("Duplicate nodes added")
+	}
+	node.Remove([]string{"a", "b", "c"})
+
+	// remove duplicate keys; should only remove once
+	node.Add([]string{"a", "b", "c"})
+	node.Remove([]string{"a", "b", "c"})
+	node.Remove([]string{"c", "b", "a"})
+	if len(node.ToSlice()) != 0 {
+		t.Fatalf("Failed to remove nodes")
 	}
 
-	//
-	node.Remove([]string{"b", "a"})
-	if node.Len() != 0 {
-		t.Fatalf("remove should have removed a node")
+	// add duplicate remote one; should leave no nodes
+	node.Add([]string{"a", "b", "c"})
+	node.Add([]string{"a", "b", "c"})
+	node.Add([]string{"a", "b", "c"})
+	node.Remove([]string{"a", "b", "c"})
+	if len(node.ToSlice()) != 0 {
+		t.Fatalf("Failed to remove nodes")
 	}
 }
 
@@ -61,121 +124,91 @@ func TestAddRemove(t *testing.T) {
 func TestList(t *testing.T) {
 	node := newNode()
 
-	//
-	node.Add([]string{"a", "b"})
+	// test simple list; length should be 1 and value should be "a"
+	node.Add([]string{"a"})
 	list := node.ToSlice()
 	if len(list) != 1 {
-		t.Fatalf("wrong length of list. Expecting 1 got %v", list)
+		t.Fatalf("Wrong number of keys - Expecting 1 got %v", len(list))
 	}
-	if len(list[0]) != 2 {
-		t.Fatalf("wrong numer of tags. Expecing 2 got %v", len(list[0]))
+	if len(list[0]) != 1 {
+		t.Fatalf("Wrong number of keys - Expecing 2 got %v", len(list[0]))
+	}
+	if strings.Join(list[0], ",") != "a" {
+		t.Fatalf("Wrong tags - Expecing 'a' got %v", list[0])
+	}
+
+	node.Add([]string{"a", "b"})
+	list = node.ToSlice()
+	if len(list) != 2 {
+		t.Fatalf("Wrong number of keys - Expecting 2 got %v", len(list))
+	}
+	if len(list[1]) != 2 {
+		t.Fatalf("Wrong number of keys - Expecing 2 got %v", len(list[1]))
+	}
+	if strings.Join(list[1], ",") != "a,b" {
+		t.Fatalf("Wrong tags - Expecing 'a,b' got %v", list[1])
+	}
+
+	node.Add([]string{"a", "b", "c"})
+	list = node.ToSlice()
+	if len(list) != 3 {
+		t.Fatalf("wrong length of list. Expecting 3 got %v", len(list))
+	}
+	if len(list[2]) != 3 {
+		t.Fatalf("Wrong number of keys - Expecing 3 got %v", len(list[2]))
+	}
+	if strings.Join(list[2], ",") != "a,b,c" {
+		t.Fatalf("Wrong tags - Expecing 'a,b,c' got %v", list[1])
 	}
 }
 
-// TestAddRemoveDuplicate
-func TestAddRemoveDuplicate(t *testing.T) {
+// TestMatchSimple
+func TestMatchSimple(t *testing.T) {
 	node := newNode()
 
-	//
-	node.Add([]string{"a", "b"})
-	node.Add([]string{"a", "b"})
-	if node.Len() != 1 {
-		t.Fatalf("duplicate add should not have added a node")
+	// simple match
+	node.Add([]string{"a"})
+	if !node.Match([]string{"a"}) {
+		t.Fatalf("Expected match!")
 	}
 
 	//
-	node.Remove([]string{"a", "b"})
-	if node.Len() != 1 {
-		t.Fatalf("duplicate remove should not have removed a node with a count > 1")
-	}
-
-	//
-	node.Remove([]string{"a", "b"})
-	if node.Len() != 0 {
-		t.Fatalf("duplicate remove should have removed a node")
-	}
-}
-
-// TestAddRemoveSimilarDuplicate
-func TestAddRemoveSimilarDuplicate(t *testing.T) {
-	node := newNode()
-
-	//
 	node.Add([]string{"a", "b"})
-	if node.Len() != 1 {
-		t.Fatalf("similar duplicate add should have added a node")
+	if !node.Match([]string{"a", "b"}) {
+		t.Fatalf("Expected match!")
 	}
 
 	//
 	node.Add([]string{"a", "b", "c"})
-	if node.Len() != 2 {
-		t.Fatalf("similar duplicate add should not have added a node")
-	}
-
-	//
-	node.Remove([]string{"a", "b"})
-	if node.Len() != 1 {
-		t.Fatalf("similar duplicate remove should not have removed a node with a count > 1")
-	}
-
-	//
-	node.Remove([]string{"a", "b", "c"})
-	if node.Len() != 0 {
-		t.Fatalf("similar duplicate remove should have removed a node")
+	if !node.Match([]string{"a", "b", "c"}) {
+		t.Fatalf("Expected match!")
 	}
 }
 
-// TestMatches
-func TestMatches(t *testing.T) {
-	matches := []matchTest{
-		{
-			[]string{},
-			[]subTest{
-				{[]string{"a"}, false},
-				{[]string{}, false},
-			},
-		},
-		{
-			[]string{"a", "b"},
-			[]subTest{
-				{[]string{}, false},
-				{[]string{"a", "b"}, true},
-				{[]string{"b", "a"}, true},
-				{[]string{"b", "a", "c"}, true},
-				{[]string{"c", "a", "b"}, true},
-				{[]string{"c", "a"}, false},
-				{[]string{"c", "b"}, false},
-				{[]string{"c"}, false},
-			},
-		},
+// TestMatchComplex
+func TestMatchComplex(t *testing.T) {
+	node := newNode()
+
+	// match unordered keys; should match
+	node.Add([]string{"a", "b", "c"})
+	if !node.Match([]string{"c", "b", "a"}) {
+		t.Fatalf("Expected match!")
 	}
 
-	for _, match := range matches {
-		node := newNode()
-		sort.Sort(sort.StringSlice(match.sub))
-
-		//
-		node.Add(match.sub)
-
-		//
-		for _, mt := range match.tests {
-			sort.Sort(sort.StringSlice(mt.test))
-			if node.Match(mt.test) != mt.result {
-				t.Fatalf("match failed: %v:%v expected %v", match.sub, mt.test, mt.result)
-			}
-		}
-
-		//
-		node.Remove(match.sub)
-
-		//
-		for _, mt := range match.tests {
-			if node.Match(mt.test) {
-				t.Fatalf("match failed: []:%v expected false", mt.test)
-			}
-		}
-
-	}
+	// match incomplete keys; should match
+	// node.Add([]string{"a", "b", "c"})
+	// if !node.Match([]string{"a"}) {
+	// 	t.Fatalf("Expected match!")
+	// }
+	// if !node.Match([]string{"a", "b"}) {
+	// 	t.Fatalf("Expected match!")
+	// }
+	// if !node.Match([]string{"b", "c"}) {
+	// 	t.Fatalf("Expected match!")
+	// }
+	// if !node.Match([]string{"a", "c"}) {
+	// 	t.Fatalf("Expected match!")
+	// }
 }
 
 // BenchmarkAddRemove
@@ -199,6 +232,186 @@ func BenchmarkMatch(b *testing.B) {
 		node.Match(keys)
 	}
 }
+
+// // TestEmptySubscription
+// func TestEmptySubscription(t *testing.T) {
+// 	node := newNode()
+// 	if node.Len() != 0 {
+// 		t.Fatalf("empty subscripition should be empty")
+// 	}
+// }
+//
+// // TestAddRemove
+// func TestAddRemove(t *testing.T) {
+// 	node := newNode()
+//
+// 	//
+// 	node.Add([]string{"a", "b"})
+// 	if node.Len() != 1 {
+// 		t.Fatalf("add should have added a node")
+// 	}
+//
+// 	//
+// 	node.Remove([]string{"a", "b"})
+// 	if node.Len() != 0 {
+// 		t.Fatalf("remove should have removed a node")
+// 	}
+//
+// 	//
+// 	node.Add([]string{"a", "b"})
+// 	if node.Len() != 1 {
+// 		t.Fatalf("add should have added a node")
+// 	}
+//
+// 	//
+// 	node.Remove([]string{"b", "a"})
+// 	if node.Len() != 0 {
+// 		t.Fatalf("remove should have removed a node")
+// 	}
+// }
+//
+// // TestList
+// func TestList(t *testing.T) {
+// 	node := newNode()
+//
+// 	//
+// 	node.Add([]string{"a", "b"})
+// 	list := node.ToSlice()
+// 	if len(list) != 1 {
+// 		t.Fatalf("wrong length of list. Expecting 1 got %v", list)
+// 	}
+// 	if len(list[0]) != 2 {
+// 		t.Fatalf("wrong numer of tags. Expecing 2 got %v", len(list[0]))
+// 	}
+// }
+//
+// // TestAddRemoveDuplicate
+// func TestAddRemoveDuplicate(t *testing.T) {
+// 	node := newNode()
+//
+// 	//
+// 	node.Add([]string{"a", "b"})
+// 	node.Add([]string{"a", "b"})
+// 	if node.Len() != 1 {
+// 		t.Fatalf("duplicate add should not have added a node")
+// 	}
+//
+// 	//
+// 	node.Remove([]string{"a", "b"})
+// 	if node.Len() != 1 {
+// 		t.Fatalf("duplicate remove should not have removed a node with a count > 1")
+// 	}
+//
+// 	//
+// 	node.Remove([]string{"a", "b"})
+// 	if node.Len() != 0 {
+// 		t.Fatalf("duplicate remove should have removed a node")
+// 	}
+// }
+//
+// // TestAddRemoveSimilarDuplicate
+// func TestAddRemoveSimilarDuplicate(t *testing.T) {
+// 	node := newNode()
+//
+// 	//
+// 	node.Add([]string{"a", "b"})
+// 	if node.Len() != 1 {
+// 		t.Fatalf("similar duplicate add should have added a node")
+// 	}
+//
+// 	//
+// 	node.Add([]string{"a", "b", "c"})
+// 	if node.Len() != 2 {
+// 		t.Fatalf("similar duplicate add should not have added a node")
+// 	}
+//
+// 	//
+// 	node.Remove([]string{"a", "b"})
+// 	if node.Len() != 1 {
+// 		t.Fatalf("similar duplicate remove should not have removed a node with a count > 1")
+// 	}
+//
+// 	//
+// 	node.Remove([]string{"a", "b", "c"})
+// 	if node.Len() != 0 {
+// 		t.Fatalf("similar duplicate remove should have removed a node")
+// 	}
+// }
+//
+// // TestMatches
+// func TestMatches(t *testing.T) {
+// 	matches := []matchTest{
+// 		{
+// 			[]string{},
+// 			[]subTest{
+// 				{[]string{"a"}, false},
+// 				{[]string{}, false},
+// 			},
+// 		},
+// 		{
+// 			[]string{"a", "b"},
+// 			[]subTest{
+// 				{[]string{}, false},
+// 				{[]string{"a", "b"}, true},
+// 				{[]string{"b", "a"}, true},
+// 				{[]string{"b", "a", "c"}, true},
+// 				{[]string{"c", "a", "b"}, true},
+// 				{[]string{"c", "a"}, false},
+// 				{[]string{"c", "b"}, false},
+// 				{[]string{"c"}, false},
+// 			},
+// 		},
+// 	}
+//
+// 	for _, match := range matches {
+// 		node := newNode()
+// 		sort.Sort(sort.StringSlice(match.sub))
+//
+// 		//
+// 		node.Add(match.sub)
+//
+// 		//
+// 		for _, mt := range match.tests {
+// 			sort.Sort(sort.StringSlice(mt.test))
+// 			if node.Match(mt.test) != mt.result {
+// 				t.Fatalf("match failed: %v:%v expected %v", match.sub, mt.test, mt.result)
+// 			}
+// 		}
+//
+// 		//
+// 		node.Remove(match.sub)
+//
+// 		//
+// 		for _, mt := range match.tests {
+// 			if node.Match(mt.test) {
+// 				t.Fatalf("match failed: []:%v expected false", mt.test)
+// 			}
+// 		}
+//
+// 	}
+// }
+//
+// // BenchmarkAddRemove
+// func BenchmarkAddRemove(b *testing.B) {
+// 	node := newNode()
+// 	keys := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
+// 	b.ResetTimer()
+// 	for i := 0; i < b.N; i++ {
+// 		node.Add(keys)
+// 		node.Remove(keys)
+// 	}
+// }
+//
+// // BenchmarkMatch
+// func BenchmarkMatch(b *testing.B) {
+// 	node := newNode()
+// 	keys := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
+// 	node.Add(keys)
+// 	b.ResetTimer()
+// 	for i := 0; i < b.N; i++ {
+// 		node.Match(keys)
+// 	}
+// }
 
 // // BenchmarkAddRemoveKeys benchmarks to see how fast mist can add/remove keys to
 // // a subscription
--- core/subscriptions.go
+++ core/subscriptions.go
@@ -1,10 +1,15 @@
 package mist
 
-const (
-	create = iota
-	remove
-	nothing
+import (
+	"fmt"
+	"sort"
 )
+
+// const (
+// 	create = iota
+// 	remove
+// 	nothing
+// )
 
 // interfaces
 type (
@@ -19,198 +24,338 @@ type (
 )
 
 // structs
+// type (
+//
+// 	// // Node ...
+// 	// Node struct {
+// 	// 	id       uint64
+// 	// 	key      string
+// 	// 	count    int
+// 	// 	children map[string]*Node
+// 	// 	parent   *Node
+// 	// 	leafs    map[uint64]*Node
+// 	// }
+//
+// )
+
+// //
+// func newNode() (child *Node) {
+// 	child = &Node{
+// 		id:       0,
+// 		count:    0,
+// 		children: map[string]*Node{},
+// 		leafs:    map[uint64]*Node{},
+// 	}
+//
+// 	//
+// 	return
+// }
+//
+// // Len ...
+// func (root *Node) Len() int {
+// 	return len(root.leafs)
+// }
+//
+// // Add ...
+// func (root *Node) Add(keys []string) {
+// 	if len(keys) == 0 {
+// 		return
+// 	}
+//
+// 	// sort tags
+// 	sort.Strings(keys)
+//
+// 	last, _ := root.traverse(keys, create)
+// 	last.count++
+// 	if last.count == 1 {
+// 		last.id = root.id
+// 		root.leafs[last.id] = last
+// 		root.id++
+// 	}
+// }
+//
+// // Remove ...
+// func (root *Node) Remove(keys []string) {
+// 	if len(keys) == 0 {
+// 		return
+// 	}
+//
+// 	// sort tags
+// 	sort.Strings(keys)
+//
+// 	found, _ := root.traverse(keys, remove)
+// 	if found != nil {
+// 		found.count--
+// 		if found.count == 0 {
+// 			delete(root.leafs, found.id)
+// 		}
+// 	}
+// }
+//
+// // Match ...
+// func (root *Node) Match(keys []string) bool {
+//
+// 	if len(keys) == 0 {
+// 		return false
+// 	}
+//
+// 	// sort tags
+// 	sort.Strings(keys)
+//
+// 	last, count := root.traverse(keys, nothing)
+// 	return last != nil && count != -1
+// }
+//
+// // ToSlice ...
+// func (root *Node) ToSlice() [][]string {
+// 	paths := make([][]string, len(root.leafs))
+// 	for idx, leaf := range root.leafs {
+// 		var path []string
+// 		for ; leaf != nil && leaf != root; leaf = leaf.parent {
+// 			path = append(path, leaf.key)
+// 		}
+// 		paths[idx] = path
+// 	}
+// 	return paths
+// }
+//
+// // Find ...
+// func (root *Node) Find(keys []string) *Node {
+//
+// 	if len(keys) == 0 {
+// 		return nil
+// 	}
+//
+// 	// sort tags
+// 	sort.Strings(keys)
+//
+// 	child, _ := root.traverse(keys, nothing)
+// 	return child
+// }
+//
+// // traverse ...
+// func (root *Node) traverse(keys []string, action int) (*Node, int) {
+// 	if len(keys) == 0 {
+// 		if root.count == 0 {
+// 			// this node is not a leaf, so return -1 so it doesn't get deleted
+// 			return root, -1
+// 		}
+// 		return root, root.count
+// 	}
+//
+// 	key := keys[0]
+// 	child, ok := root.children[key]
+//
+// 	switch action {
+// 	case remove:
+// 		if ok {
+// 			found, count := child.traverse(keys[1:], action)
+// 			if found != nil && count == 1 {
+// 				if child.count == 0 && len(child.children) == 0 {
+// 					delete(root.children, key)
+// 				}
+// 			}
+// 			return found, count
+// 		}
+//
+// 		return nil, 0
+// 	case create:
+// 		if !ok {
+// 			child = newNode()
+// 			child.parent = root
+// 			child.key = keys[0] // preserve the original key
+// 			root.children[key] = child
+// 		}
+//
+// 		return child.traverse(keys[1:], action)
+// 	default:
+// 		if ok {
+// 			found, count := child.traverse(keys[1:], action)
+// 			// if 0 or -1 indicate that the traversal didn't really work
+// 			if count != 0 {
+// 				return found, count
+// 			}
+// 		}
+//
+// 		// we didn't find a match with the key, try the rest of the keys
+// 		return root.traverse(keys[1:], action)
+// 	}
+// }
+
 type (
 
 	// Node ...
 	Node struct {
-		id       uint64
-		key      string
-		count    int
-		children map[string]*Node
-		parent   *Node
-		leafs    map[uint64]*Node
+		// toSlice  [][]string
+		branches map[string]*Node
+		leaves   map[string]struct{}
 	}
-
-	// Sub ...
-	// Sub map[string]struct{}
 )
 
-// // newSub creates a new subscription
-// func newSub() (sub Sub) {
-// 	return make(Sub)
-// }
 //
-// // Add adds tags to the subscription
-// func (sub Sub) Add(tags []string) {
-//
-// 	// sort tags
-// 	sort.Strings(tags)
-//
-// 	// join them into a string
-// 	chk := strings.Join(tags, ",")
-//
-// 	// see if the key is in the map; if not insert it
-// 	if _, found := sub[chk]; !found {
-// 		sub[chk] = struct{}{}
-// 	}
-// }
-//
-// // Remove removes tags from the subscription
-// func (sub Sub) Remove(tags []string) {
-//
-// 	// sort tags
-// 	sort.Strings(tags)
-//
-// 	// delete tags
-// 	delete(sub, strings.Join(tags, ","))
-// }
-//
-// // Match checks to see if the subscription has tags
-// func (sub Sub) Match(tags []string) (found bool) {
-//
-// 	// sort tags
-// 	sort.Strings(tags)
-//
-// 	// see if the key is in the map
-// 	_, found = sub[strings.Join(tags, ",")]
-//
-// 	//
-// 	return
-// }
-//
-// // ToSlice converts the subscription (map) to a slice of tags (slices)
-// func (sub Sub) ToSlice() (slice [][]string) {
-//
-// 	// for each key in sub...
-// 	for k := range sub {
-//
-// 		// split the key into a slice...
-// 		split := strings.Split(k, ",")
-//
-// 		// ...and append that slice to the final
-// 		slice = append(slice, split)
-// 	}
-//
-// 	//
-// 	return
-// }
+func newNode() (node *Node) {
 
-//
-func newNode() (child *Node) {
-	child = &Node{
-		id:       0,
-		count:    0,
-		children: map[string]*Node{},
-		leafs:    map[uint64]*Node{},
+	node = &Node{
+		// id:       0,
+		// count:    0,
+		branches: map[string]*Node{},
+		leaves:   map[string]struct{}{},
 	}
 
 	//
 	return
 }
 
-// Len ...
-func (root *Node) Len() int {
-	return len(root.leafs)
+// Show ...
+func (node *Node) Show(indent string) {
+	fmt.Printf("%s%#v\n", indent, node)
+	for _, branch := range node.branches {
+		branch.Show(fmt.Sprintf("  %s", indent))
+	}
 }
 
-// Add ...
-func (root *Node) Add(keys []string) {
+// Add sorts the keys and then attempts to add them
+func (node *Node) Add(keys []string) {
+
+	//
 	if len(keys) == 0 {
 		return
 	}
-	last, _ := root.traverse(keys, create)
-	last.count++
-	if last.count == 1 {
-		last.id = root.id
-		root.leafs[last.id] = last
-		root.id++
-	}
+
+	sort.Strings(keys)
+	node.add(keys)
 }
 
-// Remove ...
-func (root *Node) Remove(keys []string) {
+// add ...
+func (node *Node) add(keys []string) {
+
+	// if there is only one key remaining we are at the end of the chain, so a leaf
+	// is created
+	if len(keys) == 1 {
+		node.leaves[keys[0]] = struct{}{}
+		return
+	}
+
+	// see if there is already a branch for the first key. if not create a new one
+	// and add it; if a branch already exists we simply use it and continue
+	branch, ok := node.branches[keys[0]]
+	if !ok {
+		branch = newNode()
+		node.branches[keys[0]] = branch
+	}
+
+	// for the current branch (new or existing) continue to the next set of keys
+	// adding them as branches until there is only one left, at which point a leaf
+	// is created (above)
+	branch.add(keys[1:])
+}
+
+// Remove sorts the keys and then attempts to remove them
+func (node *Node) Remove(keys []string) {
+
+	//
 	if len(keys) == 0 {
 		return
 	}
-	found, _ := root.traverse(keys, remove)
-	if found != nil {
-		found.count--
-		if found.count == 0 {
-			delete(root.leafs, found.id)
+
+	sort.Strings(keys)
+	node.remove(keys)
+}
+
+// remove ...
+func (node *Node) remove(keys []string) {
+
+	// if there is only one key remaining we are at the end of the chain and need
+	// to remove just the leaf
+	if len(keys) == 1 {
+		delete(node.leaves, keys[0])
+		return
+	}
+
+	// see if a branch for the first key exists; if a branch exists we need to
+	// recurse down the branch until we reach the end...
+	branch, ok := node.branches[keys[0]]
+	if ok {
+
+		// continue key by key until we reach a leaf at which point its removed (above)
+		branch.remove(keys[1:])
+
+		// once we reach the end of the line, if there are no more leaves or branch
+		// on this branch, we can remove the branch
+		if len(branch.leaves) == 0 && len(branch.branches) == 0 {
+			delete(node.branches, keys[0])
 		}
 	}
 }
 
-// Match ...
-func (root *Node) Match(keys []string) bool {
-	last, count := root.traverse(keys, nothing)
-	return last != nil && count != -1
-}
+// Match sorts the keys and then attempts to find a match
+func (node *Node) Match(keys []string) bool {
 
-// ToSlice ...
-func (root *Node) ToSlice() [][]string {
-	paths := make([][]string, len(root.leafs))
-	for idx, leaf := range root.leafs {
-		var path []string
-		for ; leaf != nil && leaf != root; leaf = leaf.parent {
-			path = append(path, leaf.key)
-		}
-		paths[idx] = path
-	}
-	return paths
-}
-
-// Find ...
-func (root *Node) Find(keys []string) *Node {
-	child, _ := root.traverse(keys, nothing)
-	return child
-}
-
-// traverse ...
-func (root *Node) traverse(keys []string, action int) (*Node, int) {
+	//
 	if len(keys) == 0 {
-		if root.count == 0 {
-			// this node is not a leaf, so return -1 so it doesn't get deleted
-			return root, -1
-		}
-		return root, root.count
+		return false
 	}
 
-	key := keys[0]
-	child, ok := root.children[key]
+	sort.Strings(keys)
+	return node.match(keys)
+}
 
-	switch action {
-	case remove:
-		if ok {
-			found, count := child.traverse(keys[1:], action)
-			if found != nil && count == 1 {
-				if child.count == 0 && len(child.children) == 0 {
-					delete(root.children, key)
-				}
-			}
-			return found, count
+// ​match ...
+func (node *Node) match(keys []string) bool {
+
+	// iterate through each key looking for a leaf, if found it's a match
+	for _, key := range keys {
+		if _, ok := node.leaves[key]; ok {
+			return true
 		}
-
-		return nil, 0
-	case create:
-		if !ok {
-			child = newNode()
-			child.parent = root
-			child.key = keys[0] // preserve the original key
-			root.children[key] = child
-		}
-
-		return child.traverse(keys[1:], action)
-	default:
-		if ok {
-			found, count := child.traverse(keys[1:], action)
-			// if 0 or -1 indicate that the traversal didn't really work
-			if count != 0 {
-				return found, count
-			}
-		}
-
-		// we didn't find a match with the key, try the rest of the keys
-		return root.traverse(keys[1:], action)
 	}
+
+	// see if a branch for the first key exists; if no branch exists we need to
+	// try and find a match for the next key, continuing down the chain until we
+	// find a leaf (above)
+	branch, ok := node.branches[keys[0]]
+	if !ok {
+		return node.match(keys[1:])
+	}
+
+	// if a branch does exist we down the branch until we find a leaf (above)
+	return branch.match(keys[1:])
+}
+
+// ToSlice recurses down an entire node returning a list of all branches and leaves
+// as a slice of slices
+func (node *Node) ToSlice() (list [][]string) {
+
+	fmt.Printf("TO SLICE! %#v\n", node)
+
+	// iterate through each leaf appending it as a slice to the list of keys
+	for leaf := range node.leaves {
+		fmt.Println("LEAF?", leaf)
+		list = append(list, []string{leaf})
+	}
+
+	// iterate through each branch getting its list of branches and appending those
+	// to the list
+	for branch, node := range node.branches {
+
+		fmt.Println("BRANCH?", branch)
+
+		// get the current nodes slice of branches and leaves
+		nodeSlice := node.ToSlice()
+
+		fmt.Println("SLICE?", nodeSlice)
+
+		// for each branch in the nodes list apppend the key to that key
+		for _, nodeKey := range nodeSlice {
+			fmt.Println("WHERE AM I?", nodeKey)
+			list = append(list, append(nodeKey, branch))
+		}
+	}
+
+	// sort each list
+	for _, l := range list {
+		sort.Strings(l)
+	}
+
+	return
 }
