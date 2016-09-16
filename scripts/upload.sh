b5feae1884a3f11ebe69bf21c032728e6754c11d
Braxton Huggins <braxton@pagodabox.com>
2016-09-16 14:30:35 -0600

    invalidate cloudfront cache when uploading new binaries

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -4,3 +4,9 @@ set -e
 # upload to AWS S3
 echo "Uploading builds to S3..."
 aws s3 sync ./build/ s3://tools.nanopack.io/mist --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --region us-east-1
+
+echo "Creating invalidation for cloudfront"
+aws  configure  set preview.cloudfront true
+aws cloudfront create-invalidation \
+  --distribution-id E3B5Z3LYG19QSL \
+  --paths /mist
